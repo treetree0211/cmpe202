701ed2eaaaf6e3dcbbf99b6461a91744d347b978
Yueqiao Zhang <yueqiaozhang79@gmail.com>
2019-01-31 15:21:05 -0800

    Add 3 types machine; Add different variables for machines; Updated insert coin functions

--- lab1/gumball-java-typical/GumballMachine.java
+++ lab1/gumball-java-typical/GumballMachine.java
@@ -3,41 +3,102 @@ public class GumballMachine
 {
 
     private int num_gumballs;
-    private boolean has_quarter;
+    //private boolean has_quarter;
+    // Add quarter number to record how many quarters inserted
+    private int quarterNum;
+    // Add cost and type for different machines
+    private int cost;
+    private int type;
+    // Add current get coin sum
+    private int collectCoinSum;
 
-    public GumballMachine( int size )
+
+    // Add type number to distinguish machine type
+    /* Type 1 : accept only quarters;
+       Type 2 : accept two quarters;
+       Type 3 : accept all coins;
+    */
+    public GumballMachine( int size, int type)
     {
         // initialise instance variables
         this.num_gumballs = size;
-        this.has_quarter = false;
+        //this.has_quarter = false;
+        this.type = type;
+        this.collectCoinSum = 0;
+        this.quarterNum = 0;
+        if (this.type == 1) this.cost = 25;
+        else {
+          this.cost = 50; 
+        }
     }
 
-    public void insertQuarter(int coin)
+    // check if quarter or other coin inserted
+    public void insertCoin(int coin)
     {
-        if ( coin == 25 )
-            this.has_quarter = true ;
-        else 
-            this.has_quarter = false ;
+        //check if contains one or two quarter
+        if ( coin == 25 ) this.quarterNum++;
+
+        this.collectCoinSum += coin;
+    }
+
+    public int getCoinSum() {
+        return this.collectCoinSum;
     }
     
     public void turnCrank()
     {
-    	if ( this.has_quarter )
-    	{
-    		if ( this.num_gumballs > 0 )
-    		{
-    			this.num_gumballs-- ;
-    			this.has_quarter = false ;
-    			System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
-    		}
-    		else
-    		{
-    			System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
-    		}
-    	}
-    	else 
-    	{
-    		System.out.println( "Please insert a quarter" ) ;
-    	}        
+        //type 1
+        if (this.type == 1) {
+            if ( this.quarterNum == 1 )
+            {
+                if ( this.num_gumballs > 0 )
+                {
+                    // after enject gumball, set value to default
+                    this.num_gumballs-- ;
+                    this.collectCoinSum = 0 ;
+                    this.quarterNum = 0;
+                    System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
+                }
+                else
+                {
+                    System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
+                }
+            }
+            
+        } else if (this.type == 2) {
+            if (this.quarterNum == 2) {
+                if ( this.num_gumballs > 0 )
+                {
+                    this.num_gumballs-- ;
+                    this.quarterNum = 0;
+                    this.collectCoinSum = 0 ;
+                    System.out.println( "Thanks for your 2 quarters.  Gumball Ejected!" ) ;
+                }
+                else
+                {
+                    System.out.println( "No More Gumballs!  Sorry, can't return your 2 quarters." ) ;
+                }
+
+            } else {
+                System.out.println( "Please insert a quarter" ) ;
+            } 
+        } else {
+            int sum = getCoinSum();
+            if (sum == this.cost) {
+                if ( this.num_gumballs > 0 )
+                {
+                    this.num_gumballs-- ;
+                    this.collectCoinSum = 0 ;
+                    this.quarterNum = 0;
+                    System.out.println( "Thanks for your coins.  Gumball Ejected!" ) ;
+                }
+                else
+                {
+                    System.out.println( "No More Gumballs!  Sorry, can't return your coin." ) ;
+                }
+            } else {
+                System.out.println( "Please insert a coin" ) ;
+            } 
+        }
     }
 }
--- lab1/gumball-java-typical/Main.java
+++ lab1/gumball-java-typical/Main.java
@@ -3,20 +3,42 @@
 public class Main {
 
 	public static void main(String[] args) {
-		GumballMachine gumballMachine = new GumballMachine(5);
+		// initialize type 1 machine and insert 1 quarter
+		GumballMachine gumballMachine1 = new GumballMachine(5, 1);
 
-		System.out.println(gumballMachine);
+		System.out.println(gumballMachine1);
 
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
+		gumballMachine1.insertCoin( 25 );
+		gumballMachine1.turnCrank();
 
-		System.out.println(gumballMachine);
+		System.out.println(gumballMachine1);
 
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
+		gumballMachine1.insertCoin( 25 );
+		gumballMachine1.turnCrank();
+		gumballMachine1.insertCoin( 25 );
+		gumballMachine1.turnCrank();
 
-		System.out.println(gumballMachine);
+		// initialize type 2 machine and insert 2 quarter
+		GumballMachine gumballMachine2 = new GumballMachine(5, 2);
+		System.out.println(gumballMachine2);
+		gumballMachine2.insertCoin( 25 );
+		gumballMachine2.insertCoin( 25 );
+		gumballMachine2.turnCrank();
+
+		System.out.println(gumballMachine2);
+		// test a failed case : we can not get a gumball 
+		gumballMachine2.insertCoin( 25 );
+		gumballMachine2.insertCoin( 10 );
+		gumballMachine2.turnCrank();
+
+		// initialize type 3 machine and insert different coins
+		GumballMachine gumballMachine3 = new GumballMachine(5, 3);
+		System.out.println(gumballMachine3);
+		gumballMachine3.insertCoin( 10 );
+		gumballMachine3.insertCoin( 5 );
+		gumballMachine3.insertCoin( 25 );
+		gumballMachine3.insertCoin( 10 );
+		gumballMachine3.turnCrank();
+
 	}
 }
